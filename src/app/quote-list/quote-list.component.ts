8f37ec2be320a4050f6fd168bd5dd9a45801276d
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-24 14:37:08 -0400

    Quote list function added

--- src/app/quote-list/quote-list.component.ts
+++ src/app/quote-list/quote-list.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteListComponent implements OnInit {
 
+  quotes: Array<any> = [
+    {
+        id: 1,
+        text: 'I love fries.',
+        author: 'Neil Armstrong'
+    }, {
+        id: 12,
+        text: 'Stay thirsty, my friends. Dos Equis.',
+        author: 'Most Interesting Man In The World'
+    }, {
+        id: 99,
+        text: 'Stay hungry. Stay foolish.',
+        author: 'Ghost of Steve Jobs'
+    }, {
+        id: 101,
+        text: 'Do things that don\'t scale.',
+        author: 'Steve Jobs'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  removeQuote (id) {
+    this.quotes = this.quotes.filter(
+      (oneQuote) => oneQuote.id !== id
+    );
+  }
 }
